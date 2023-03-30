67e320fd17d9340c65327f817725a462c310680d
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2023-03-30 15:34:18 -0400

    lab 4 fini

--- Lab 4/processeur_mem_interface.vhd
+++ Lab 4/processeur_mem_interface.vhd
@@ -18,17 +18,18 @@ ENTITY processeur_mem_interface IS
 END processeur_mem_interface;
 
 ARCHITECTURE arch OF processeur_mem_interface IS
-	SIGNAL clk, rst : STD_LOGIC := '0';
+	SIGNAL run, clk, rst : STD_LOGIC;
 	SIGNAL buswire, din : STD_LOGIC_VECTOR(8 DOWNTO 0);
 	SIGNAL done : STD_LOGIC;
 	SIGNAL address, nextAddress : STD_LOGIC_VECTOR(4 DOWNTO 0) := "00000";
 BEGIN
+	run <= SW(0);
 	d_key0 : ENTITY work.debounce PORT MAP(CLOCK_50, KEY(0), clk);
 	d_key1 : ENTITY work.debounce PORT MAP(CLOCK_50, KEY(1), rst);
 
-	processeur0 : processeur PORT MAP(SW(0), clk, rst, din, buswire, done);
+	processeur0 : processeur PORT MAP(run, clk, rst, din, buswire, done);
 
-	LEDR <= "000000000" & done;
+	LEDR <= address & "0000" & done;
 
 	sseg0 : sseg PORT MAP(buswire(3 DOWNTO 0), HEX0);
 	sseg1 : sseg PORT MAP(buswire(7 DOWNTO 4), HEX1);
@@ -38,6 +39,6 @@ BEGIN
 	sseg5 : sseg PORT MAP("0" & din(8 DOWNTO 6), HEX5);
 
 	nextAddress <= address + "00001";
-	counter : registre GENERIC MAP(5) PORT MAP(nextAddress, clk, done, address);
+	counter : registre GENERIC MAP(5) PORT MAP(nextAddress, clk, done and run, address);
 	mem : inst_mem PORT MAP(address, clk, din);
 END arch;
